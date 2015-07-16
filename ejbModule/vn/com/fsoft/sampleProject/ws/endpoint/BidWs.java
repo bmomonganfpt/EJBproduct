a2655c04d4cf7769aa96cb09adfb3894c6d142a5
Brian Momongan <bmomonganfpt@gmail.com>
2015-07-16 15:53:48 +0800

    with input

--- ejbModule/vn/com/fsoft/sampleProject/ws/endpoint/BidWs.java
+++ ejbModule/vn/com/fsoft/sampleProject/ws/endpoint/BidWs.java
@@ -2,6 +2,7 @@ package vn.com.fsoft.sampleProject.ws.endpoint;
 
 import java.util.List;
 
+import javax.ejb.EJB;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.jws.WebParam;
@@ -9,6 +10,8 @@ import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.Style;
 
+import vn.com.fsoft.sampleProject.business.Bidding;
+import vn.com.fsoft.sampleProject.business.BiddingLocal;
 import vn.com.fsoft.sampleProject.entities.Account;
 import vn.com.fsoft.sampleProject.entities.Bid;
 import vn.com.fsoft.sampleProject.entities.Item;
@@ -26,20 +29,24 @@ public class BidWs implements BidWsRemote {
 	/**
 	 * Default constructor.
 	 */
+
+	@EJB
+	Bidding bidding;
+
 	public BidWs() {
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public List<Bid> getBidding(@WebParam(name="Account") Account account) {
-		// TODO Auto-generated method stub
+	public List<Bid> getBidding(@WebParam(name = "Account") Account account) {
+
 		return null;
 	}
 
 	@Override
-	public Item getItem(Account account) {
-		// TODO Auto-generated method stub
-		return null;
+	public Item getItem(@WebParam(name = "Account") Account account) {
+
+		return bidding.getItem(account);
 	}
 
 }
